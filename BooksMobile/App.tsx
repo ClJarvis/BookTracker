4d8bbee945faa37617d92d342f7c8c7c15cb54a6
Chris <clj@ollure.com>
2023-03-01 12:54:47 -0600

    added query

--- BooksMobile/App.tsx
+++ BooksMobile/App.tsx
@@ -6,8 +6,7 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-const API_KEY = "neueneustadt::stepzen.net+1000::739ac5baadd9780ac6f613b44ad1599fbf840b38143f9c966f6efd96acbe6685
-";
+const API_KEY = 'neueneustadt::stepzen.net+1000::739ac5baadd9780ac6f613b44ad1599fbf840b38143f9c966f6efd96acbe6685';
 
 const client = new ApolloClient({
   uri: "<https://neueneustadt.stepzen.net/api/plundering-dragonfly/__graphql>",
