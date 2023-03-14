d33ef1b37631f2eeec31c773586b24360b9db7df
Chris <clj@ollure.com>
2023-03-14 18:00:11 -0500

    creating a book list

--- BooksMobile/context/MyBooksProvider.tsx
+++ BooksMobile/context/MyBooksProvider.tsx
@@ -1,11 +1,46 @@
 import {createContext, useContext} from 'react';
 
-const MyBooksContext = createContext({});
+type MyBooksContextType = {
+	onToggleSaved: (book: Book) => void;
+	IsBookSaved: (book: Book) => boolean;
+	savedBooks: Book[];
+};
 
+const MyBooksContext = createContext<MyBooksContextType>s({
+	onToggleSaved: () => {};
+	isBookSaved: () => false;
+	savedBooks: [],
+});
 
-const MyBooksProvider = ({children}) => {
+type Props = {
+	children: ReactNode;
+};
+
+const MyBooksProvider = ({children}: Props) => {
+	const [savedBooks, setSavedBooks] = useState,Book[]>([]);
+
+	const areBooksTheSame = (a: Book, b: Book) => {
+	return JSON.stringify(a) === JSON.stringify(b);
+	}
+
+	const isBookSaved = (book, Book) => {
+		return savedBooks.some[(savedBook) => areBooksTheSame(savedBook, book));
+	}
+
+	const onToggleSaved = (book: Book) => {
+		if (isBookSaved(Book)) {
+
+			setSavedBooks{(books) => books.filter((savedBooks) => !areBooksTheSame(savedBook, book))
+			);s
+
+		} else {
+			setSavedBooks{(books) => [book, ...books]};
+		}
+
+	};
+
 	return (
-		<MyBooksContext.Provider value={{test : "Hello there!"}}>
+		<MyBooksContext.Provider value={{ onToggleSaved, isBookSaved, savedBooks }}>
 		{children}
 		</MyBooksContext.Provider>
 	);
