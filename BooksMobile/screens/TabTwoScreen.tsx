ceb9c105f163a5c2100d7eb851b59c92474cc631
Chris <clj@ollure.com>
2023-03-21 18:53:59 -0500

    selcted books added to list and appear on second tab

--- BooksMobile/screens/TabTwoScreen.tsx
+++ BooksMobile/screens/TabTwoScreen.tsx
@@ -1,14 +1,20 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, FlatList, } from 'react-native';
 
 import { View } from '../components/Themed';
 
 import { useMyBooks } from '../context/MyBooksProvider';
+import BookItem from "../components/BookItem";
 
 export default function TabTwoScreen() {
-  const { test } = useMyBooks();
+  const { savedBooks } = useMyBooks();
+  console.log(savedBooks);
+
   return (
     <View style={styles.container}>
-
+      <FlatList 
+        data={savedBooks}
+        renderItem={({ item }) => <BookItem book={item} />}
+      />
     </View>
   );
 }
@@ -16,8 +22,7 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
   },
   title: {
     fontSize: 20,
