cd622e35e3be31e5d8523febb064271b2c42b1f9
Chris <clj@ollure.com>
2024-01-31 12:29:34 -0600

    total pages read display on page.

--- BooksMobile/screens/TabTwoScreen.tsx
+++ BooksMobile/screens/TabTwoScreen.tsx
@@ -4,14 +4,27 @@ import { View } from '../components/Themed';
 
 import { useMyBooks } from '../context/MyBooksProvider';
 import BookItem from "../components/BookItem";
+import BooksRead from 'Booksread';
 
 export default function TabTwoScreen() {
   const { savedBooks } = useMyBooks();
   console.log(savedBooks);
-
+/*
 const pagesRead = (sum => value => sum += value)(0);
-const totalPagesRead = pagesRead ;
-console.log(savedBooks.length);
+const totalPagesRead = pagesRead; */
+
+/*const pagesRead = savedBooks.reduce((accumulator ,savedBooks) => {
+  return accumulator += savedBooks.pagesRead;
+}, 0) */
+
+let totalPagesRead = 1;
+savedBooks.forEach(savedBooks => {
+  totalPagesRead += savedBooks.pages;
+})
+
+
+console.log(totalPagesRead);
+console.log(savedBooks[1].pages);
 
   return (
     <View style={styles.container}>
@@ -19,7 +32,7 @@ console.log(savedBooks.length);
         data={savedBooks}
         renderItem={({ item }) => <BookItem book={item} />}
       />
-          <Text style={{fontSize: '1.5rem', background:'rgb(70, 170, 113)', padding: 6,}}>    Number of Books Read: {savedBooks.length}</Text>
+          <Text style={{fontSize: '1.5rem', background:'rgb(70, 170, 113)', padding: 6,}}>    Number of Books Read: {savedBooks.length} <br /> Total Pages read: {totalPagesRead}</Text> 
     </View>
   );
 }
